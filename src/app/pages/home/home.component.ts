9712235fa0a16f350fb4ac2dd1cc7c931bbacc02
Klajdi Hoxha <klainti95@gmail.com>
2021-11-07 21:52:07 +0100

    home page search functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { ApiService } from 'src/app/core/services/api.service';
+import { Client } from 'src/app/core/models/client.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  data: Client | any;
+  searchTerm: String = '';
+  result: Boolean = false;
+
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+  }
+
+  search(){
+    this.api.get('/client/' + this.searchTerm).subscribe(
+      (data) => {
+        this.data = data;
+        if(this.data && this.searchTerm !== ''){
+          this.result = true;
+        }else{
+          this.result = false;
+        }
+        console.log(this.data);
+      }
+    );
   }
 
 }
